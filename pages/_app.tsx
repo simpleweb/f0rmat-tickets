55ab6c23daf090ed08e063aaacf071fad6f520a9
RuariM <48099261+RuariM@users.noreply.github.com>
2022-04-28 20:54:27 +0100

    fixed connect popup spam

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,7 +46,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     if (previouslyConnectedWallets?.length) {
       async function setWalletFromLocalStorage() {
-        await connect({ autoSelect: previouslyConnectedWallets[0] });
+        await connect({
+          autoSelect: {
+            label: previouslyConnectedWallets[0],
+            disableModals: true,
+          },
+        });
       }
       setWalletFromLocalStorage();
     }
